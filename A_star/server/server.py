d221c9d5f45b15e6cadbdfaa7e3aedc8330697b4
Alok Mysore <alok.shankar.m@gmail.com>
2018-02-13 01:56:45 -0800

    some more

--- A_star/server/server.py
+++ A_star/server/server.py
@@ -47,4 +47,4 @@ def grid_exec():
     return jsonify(paths)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
--- A_star/visualization/app/scripts/controllers/main.js
+++ A_star/visualization/app/scripts/controllers/main.js
@@ -10,33 +10,33 @@
 
 angular.module('visualizationApp')
     .controller('MainCtrl', function ($scope, $http) {
-        $scope.resetGrid = function(x, y){
+        $scope.x = 5;
+        $scope.y = 10;
+
+        $scope.resetGrid = function(){
             $scope.start = null;
             $scope.paths = null;
             $scope.step = 0;
-
             $scope.activeTool = 'shelf';
-            $scope.x = 5;
-            $scope.y = 10;
             $scope.grid = [];
             for (var i = 0; i < $scope.x; i++) {
                 $scope.grid[i] = [];
                 for (var j = 0; j < $scope.y; j++) {
                     $scope.grid[i][j] = {
-                        'type': 'floor',
-                        'x': i,
-                        'y': j,
-                        'path': false
+                        type: 'floor',
+                        x: i,
+                        y: j,
+                        path: false
                     };
                 }
             }
-        }
+        };
 
         function setStart(x, y){
             if($scope.start !== null){
-                var old_x = $scope.start.x
-                var old_y = $scope.start.y
-                $scope.grid[old_x][old_y].type = 'floor';
+                var oldX = $scope.start.x;
+                var oldY = $scope.start.y;
+                $scope.grid[oldX][oldY].type = 'floor';
             }
             $scope.start = {x: x, y: y};
             $scope.grid[x][y].type = 'start';
@@ -82,29 +82,32 @@ angular.module('visualizationApp')
             if($scope.activeTool === 'end'){
                 setEnd(cell.x, cell.y);
             }
-        }
+        };
 
-        $scope.setPath = function(){
+        $scope.resetPaths = function(){
             for (var i = 0; i < $scope.x; i++) {
                 for (var j = 0; j < $scope.y; j++) {
                     $scope.grid[i][j].path = false;
                 }
             }
+        };
 
+        $scope.setPath = function(){
+            $scope.resetPaths();
             for (var i = 1; i < $scope.paths[$scope.step].length; i++) {
-                var box = $scope.paths[$scope.step][i]
+                var box = $scope.paths[$scope.step][i];
                 $scope.grid[box[0]][box[1]].path = true;
             }
-        }
+        };
 
         $scope.setStep = function(step){
             $scope.step = step;
             $scope.setPath();
-        }
+        };
 
         $scope.findPath = function(){
-            var walls = []
-            var ends = []
+            var walls = [];
+            var ends = [];
             for (var i = 0; i < $scope.x; i++) {
                 for (var j = 0; j < $scope.y; j++) {
                     if($scope.grid[i][j].type === 'end'){
@@ -117,19 +120,20 @@ angular.module('visualizationApp')
             }
 
             var payload = {
-                "start": [$scope.start.x, $scope.start.y], 
-                "end": ends,
-                "rows": $scope.x,
-                "cols": $scope.y,
-                "walls": walls
-            }
-            var url = 'http://127.0.0.1:5000?info=' + JSON.stringify(payload)
+                start: [$scope.start.x, $scope.start.y], 
+                end: ends,
+                rows: $scope.x,
+                cols: $scope.y,
+                walls: walls
+            };
+
+            var url = 'http://127.0.0.1:5000?info=' + JSON.stringify(payload);
 
             $http.get(url).then(function(response){
                 $scope.paths = response.data;
                 $scope.setStep(0);
             });
-        }
+        };
 
         $scope.resetGrid();
     });
