1593560c57bbedfbc137809e67048f46a5b15ad9
Alok Mysore <alok.shankar.m@gmail.com>
2018-02-12 23:24:07 -0800

    viz changes

--- A_star/visualization/app/scripts/controllers/main.js
+++ A_star/visualization/app/scripts/controllers/main.js
@@ -9,116 +9,128 @@
  */
 
 angular.module('visualizationApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.start = null;
-    $scope.end = null;
-    $scope.activeTool = 'shelf';
-    $scope.x = 5;
-    $scope.y = 10;
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.resetGrid = function(x, y){
+            $scope.start = null;
+            $scope.paths = null;
+            $scope.step = null;
 
-    $scope.resetGrid = function(x, y){
-      $scope.grid = [];
-      for (var i = 0; i < $scope.x; i++) {
-        $scope.grid[i] = [];
-        for (var j = 0; j < $scope.y; j++) {
-          $scope.grid[i][j] = {
-            'type': 'floor',
-            'x': i,
-            'y': j
-          };
+            $scope.activeTool = 'shelf';
+            $scope.x = 5;
+            $scope.y = 10;
+            $scope.grid = [];
+            for (var i = 0; i < $scope.x; i++) {
+                $scope.grid[i] = [];
+                for (var j = 0; j < $scope.y; j++) {
+                    $scope.grid[i][j] = {
+                        'type': 'floor',
+                        'x': i,
+                        'y': j
+                    };
+                }
+            }
         }
-      }
-    }
 
-    function setStart(x, y){
-      if($scope.start !== null){
-        var old_x = $scope.start.x
-        var old_y = $scope.start.y
-        $scope.grid[old_x][old_y].type = 'floor';
-      }
-      $scope.start = {x: x, y: y};
-      $scope.grid[x][y].type = 'start';
-    }
-
-    function setEnd(x, y){
-      if($scope.end !== null){
-        var old_x = $scope.end.x
-        var old_y = $scope.end.y
-        $scope.grid[old_x][old_y].type = 'floor';
-      }
-      $scope.end = {x: x, y: y};
-      $scope.grid[x][y].type = 'end';
-
-    }
-
-    function setFloor(x, y){
-      if($scope.grid[x][y].type === 'start'){
-        $scope.start = null;
-      }
-      if($scope.grid[x][y].type === 'end'){
-        $scope.end = null;
-      }
-
-      $scope.grid[x][y].type = 'floor';
-    }
-
-    function setShelf(x, y){
-      if($scope.grid[x][y].type === 'start'){
-        $scope.start = null;
-      }
-      if($scope.grid[x][y].type === 'end'){
-        $scope.end = null;
-      }
-
-      $scope.grid[x][y].type = 'shelf';
-    }
-
-    $scope.squareClick = function(cell){
-      if($scope.activeTool === 'shelf'){
-        setShelf(cell.x, cell.y);
-      }
-
-      if($scope.activeTool === 'floor'){
-        setFloor(cell.x, cell.y);
-      }
-
-      if($scope.activeTool === 'start'){
-        setStart(cell.x, cell.y);
-      }
-
-      if($scope.activeTool === 'end'){
-        setEnd(cell.x, cell.y);
-      }
-    }
-
-    $scope.findPath = function(){
-      var walls = []
-      for (var i = 0; i < $scope.x; i++) {
-        for (var j = 0; j < $scope.y; j++) {
-          if($scope.grid[i][j].type === 'shelf'){
-            walls.push([i, j]);
-          }
+        function setStart(x, y){
+            if($scope.start !== null){
+                var old_x = $scope.start.x
+                var old_y = $scope.start.y
+                $scope.grid[old_x][old_y].type = 'floor';
+            }
+            $scope.start = {x: x, y: y};
+            $scope.grid[x][y].type = 'start';
         }
-      }
 
-      var payload = {
-        "start": [$scope.start.x, $scope.start.y], 
-        "end": [$scope.end.x, $scope.end.y],
-        "rows": $scope.x,
-        "cols": $scope.y,
-        "walls": walls
-      }
-      var url = 'http://127.0.0.1:5000?info=' + JSON.stringify(payload)
+        function setEnd(x, y){
+            if($scope.grid[x][y].type === 'start'){
+                $scope.start = null;
+            }
 
-      $http.get(url).then(function(response){
-        for (var i = 1; i < response.data.length - 1; i++) {
-          var x = response.data[i][0];
-          var y = response.data[i][1];
-          $scope.grid[x][y].type = 'path';
+            $scope.grid[x][y].type = 'end';
         }
-      });
-    }
 
-    $scope.resetGrid();
+        function setFloor(x, y){
+            if($scope.grid[x][y].type === 'start'){
+                $scope.start = null;
+            }
 
-  });
+            $scope.grid[x][y].type = 'floor';
+        }
+
+        function setShelf(x, y){
+            if($scope.grid[x][y].type === 'start'){
+                $scope.start = null;
+            }
+
+            $scope.grid[x][y].type = 'shelf';
+        }
+
+        $scope.squareClick = function(cell){
+            if($scope.activeTool === 'shelf'){
+                setShelf(cell.x, cell.y);
+            }
+
+            if($scope.activeTool === 'floor'){
+                setFloor(cell.x, cell.y);
+            }
+
+            if($scope.activeTool === 'start'){
+                setStart(cell.x, cell.y);
+            }
+
+            if($scope.activeTool === 'end'){
+                setEnd(cell.x, cell.y);
+            }
+        }
+
+        $scope.setPath = function(){
+            for (var i = 0; i < $scope.x; i++) {
+                for (var j = 0; j < $scope.y; j++) {
+                    if($scope.grid[i][j].type === 'path'){
+                        $scope.grid[i][j].type = 'floor';
+                    }
+                }
+            }
+
+            for (var i = 0; i < $scope.paths[$scope.step].length; i++) {
+                var box = $scope.paths[$scope.step][i]
+                $scope.grid[box[0]][box[1]].type = 'path';
+            }
+        }
+
+        $scope.setStep = function(step){
+            $scope.step = step;
+            $scope.setPath();
+        }
+
+        $scope.findPath = function(){
+            var walls = []
+            var ends = []
+            for (var i = 0; i < $scope.x; i++) {
+                for (var j = 0; j < $scope.y; j++) {
+                    if($scope.grid[i][j].type === 'end'){
+                        ends.push([i, j]);
+                    }
+                    if($scope.grid[i][j].type === 'shelf'){
+                        walls.push([i, j]);
+                    }
+                }
+            }
+
+            var payload = {
+                "start": [$scope.start.x, $scope.start.y], 
+                "end": ends,
+                "rows": $scope.x,
+                "cols": $scope.y,
+                "walls": walls
+            }
+            var url = 'http://127.0.0.1:5000?info=' + JSON.stringify(payload)
+
+            $http.get(url).then(function(response){
+                $scope.paths = response;
+                $scope.setStep(0);
+            });
+        }
+
+        $scope.resetGrid();
+    });
