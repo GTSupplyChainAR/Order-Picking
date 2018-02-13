c3d47a0686f4781bca39f2746ac6087557ac90ca
Chiraag Sumanth <chiraagsumanth@Chiraags-MacBook-Pro.local>
2018-02-12 23:27:32 -0800

    Updated server for multiple picks

--- A_star/server/server.py
+++ A_star/server/server.py
@@ -8,6 +8,19 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
+def get_shortest(start, ends, walls, r, c):
+
+    min_l = float('+inf')
+    for i in ends:
+        a = pf.AStar()
+        a.init_grid(r, c, walls, start, i)
+        path = a.solve()
+        if len(path) < min_l:
+            min_l = len(path)
+            path_ret = path
+
+    return path
+
 @app.route('/', methods=['GET'])
 def grid_exec():
     raw_str = request.args['info']
@@ -16,14 +29,22 @@ def grid_exec():
     r = json_str['rows']
     c = json_str['cols']
     start = tuple(json_str['start'])
-    end = tuple(json_str['end'])
+    end = json_str['end']
+    end = [tuple(x) for x in end]
     walls = tuple([tuple(x) for x in json_str['walls']])
 
-    a = pf.AStar()
-    a.init_grid(r, c, walls, start, end)
-    path = a.solve()
+    whl_ctr = len(end)
 
-    return jsonify(path)
+    paths = []
+    ctr = 0
+    while ctr < whl_ctr:
+        shortest_p = get_shortest(start, end, walls, r, c)
+        paths.append(shortest_p)
+        start = paths[-1][-1]
+        end.remove(start)
+        ctr += 1
+
+    return jsonify(paths)
 
 if __name__ == '__main__':
     app.run(debug=True)
